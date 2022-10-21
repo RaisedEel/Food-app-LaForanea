fa94d33ee13d6b9db291fb1a6d7332444b99c930
Antonio Aguilera <antonhy.romero98@gmail.com>
2022-10-21 00:39:26 -0500

    Retocado los estilos del componente DishItem, sobre todo el form. Agregada una imagen a assets.

--- src/components/dishes/DishItem.js
+++ src/components/dishes/DishItem.js
@@ -4,19 +4,28 @@ function DishItem(props) {
   return (
     <li>
       <article className={classes.dish}>
-        <img
-          className={classes['dish-img']}
-          src={props.image}
-          alt={props.name}
-        />
+        <div className={classes['dish-img-container']}>
+          <img
+            className={classes['dish-img']}
+            src={props.image}
+            alt={props.name}
+          />
+        </div>
         <div className={classes['dish-header']}>
           <p className={classes['dish-title']}>{props.name}</p>
           <span className={classes['dish-price']}>{props.price}</span>
         </div>
         <p className={classes['dish-description']}>{props.description}</p>
-        <form className={classes['dish-form']} action='#'>
-          <input type='number' />
-          <button>Ordenar!</button>
+        <form
+          className={`${classes['dish-form']} grid grid--2-cols`}
+          action='#'
+        >
+          <input
+            className={classes['dish-input']}
+            type='number'
+            defaultValue='1'
+          />
+          <button className='btn'>Ordenar!</button>
         </form>
       </article>
     </li>
--- src/components/dishes/DishList.js
+++ src/components/dishes/DishList.js
@@ -1,15 +1,15 @@
 import DishItem from './DishItem';
 import classes from './DishList.module.css';
+import hamImg from '../../assets/hamburger.jpg';
 
 const dishes = [
   {
     id: 1,
-    name: 'Hamburguesa 1',
+    name: 'Hamburguesa con Queso y Tomate',
     price: '$50',
     description:
       'Suspendisse mollis turpis eu sapien sagittis tristique. In dictum ligula non dui sodales elementum. Quisque vitae turpis tincidunt, ultricies velit eu, aliquam turpis.Quisque vitae turpis tincidunt, ultricies velit eu, aliquam turpis.',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/4/4d/Cheeseburger.jpg',
+    image: hamImg,
   },
   {
     id: 2,
@@ -17,16 +17,14 @@ const dishes = [
     price: '$100',
     description:
       'Suspendisse mollis turpis eu sapien sagittis tristique. In dictum ligula non dui sodales elementum. Quisque vitae turpis tincidunt, ultricies velit eu, aliquam turpis.',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/4/4d/Cheeseburger.jpg',
+    image: hamImg,
   },
   {
     id: 3,
     name: 'Hamburguesa 3',
     price: '$25',
     description: 'Suspendisse mollis turpis eu sapien sagittis tristique.',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/4/4d/Cheeseburger.jpg',
+    image: hamImg,
   },
   {
     id: 4,
@@ -34,8 +32,7 @@ const dishes = [
     price: '$50',
     description:
       'Suspendisse mollis turpis eu sapien sagittis tristique. In dictum ligula non dui sodales elementum.',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/4/4d/Cheeseburger.jpg',
+    image: hamImg,
   },
 ];
 
