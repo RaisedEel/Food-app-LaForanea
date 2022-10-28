959ab16480a4e8d6ff77e1484acfcba9e7f666ac
Antonio Aguilera <antonhy.romero98@gmail.com>
2022-10-27 21:18:16 -0500

    Agregados media queries para hacer responsiva la app. Los componentes mas afectados son index, RestaurantAccordion y los Dish components.

--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -1,14 +1,28 @@
+import { useEffect, useState } from 'react';
 import Cart from '../components/cart/Cart';
 import Menu from '../components/menu/Menu';
 import RestaurantAccordion from '../components/menu/RestaurantAccordion';
 
 function MenuPage() {
+  const [showCart, setShowCart] = useState(window.innerWidth > 992);
+
+  function showCartHandler() {
+    setShowCart(window.innerWidth > 992);
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', showCartHandler);
+    return () => {
+      window.removeEventListener('resize', showCartHandler);
+    };
+  }, []);
+
   return (
     <div className='container'>
       <RestaurantAccordion />
-      <div className='grid' style={{ gridTemplateColumns: '1fr 45rem' }}>
+      <div className='grid grid--menu'>
         <Menu />
-        <Cart />
+        {showCart && <Cart />}
       </div>
     </div>
   );
