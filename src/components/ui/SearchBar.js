173c779f68f6dc7f4d21161e2a8080188f903cbf
Antonio Aguilera <antonhy.romero98@gmail.com>
2022-10-29 00:35:27 -0500

    Componentes Layout creados: un header y una barra de busqueda entre lo mas importante. Modificaciones menores a los estilos para acomodar a los nuevos componentes.

---
+++ src/components/ui/SearchBar.js
@@ -0,0 +1,27 @@
+import classes from './SearchBar.module.css';
+
+function SearchBar(props) {
+  return (
+    <div className={`${classes.searchbar} ${props.className}`}>
+      <button className={classes['search-btn']}>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className={classes['search-icon']}
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
+          />
+        </svg>
+      </button>
+      <input className={classes['search-input']} type='search' />
+    </div>
+  );
+}
+
+export default SearchBar;
